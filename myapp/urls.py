0028c5b65f883682c4fc19e86c11bbdcfa512d54
Than Than Soe <thanthansoe15151@gmail.com>
2023-06-13 12:18:25 +0630

    file replace

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.main, name='main_page'),
+    path('hero/', views.hero, name='heropage'),
+    path('about/', views.about, name='aboutpage'),
+    path('service/', views.service, name='servicepage'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -2,3 +2,12 @@ from django.shortcuts import render, redirect
 
 def main(request):
     return render(request, 'pages/main.html')
+
+def hero(request):
+    return render(request, 'pages/hero.html')
+
+def about(request):
+    return render(request, 'pages/about.html')
+
+def service(request):
+    return render(request, 'pages/services.html')
