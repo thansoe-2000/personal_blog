bb6081686c0f61288d1c49b9b67584eea22e781d
Than Than Soe <thanthansoe15151@gmail.com>
2023-06-21 12:31:55 +0630

    delete backend code

--- myapp/forms.py
+++ myapp/forms.py
@@ -4,18 +4,3 @@ from django.forms import ModelForm
 
 
 
-
-class HeroForm(ModelForm):
-    class Meta:
-        model = Hero
-        fields = "__all__"
-
-class AboutForm(ModelForm):
-    class Meta:
-        model = About
-        fields ="__all__"
-
-class SkillForm(ModelForm):
-    class Meta:
-        model = Skill
-        fields = "__all__"
