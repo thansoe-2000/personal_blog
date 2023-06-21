dd3e353945a34093475a5ac54c2b3ca07d8bfe68
Than Than Soe <thanthansoe15151@gmail.com>
2023-06-21 13:04:27 +0630

    add photos in projects

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('main-page', views.main, name='main_page'),
+    
     path('', views.home, name='homepage'),
     path('about/', views.about, name='aboutpage'),
     path('project/', views.project, name='projectpage'),
@@ -10,10 +10,5 @@ urlpatterns = [
     
 
 
-    # back admin-dashboard
-    path('dashboard/', views.index, name='index_dashboard'),
-    path('category/', views.category, name='category_page'),
-    path('back/hero/', views.adminhero, name='hero_admin'),
-    path('back/about/', views.adminabout, name='about_admin'),
-    path('back/skill/', views.skilladmin, name='skill_admin'),
+   
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,18 +1,14 @@
 from django.shortcuts import render, redirect
 from . models import *
 from . forms import *
-def main(request):
-    return render(request, 'pages/index.html')
+
 
 def home(request):
     return render(request, 'projects/home.html')
 
 def about(request):
-    abouts = About.objects.first()
-    context = {
-        'abouts':abouts
-    }
-    return render(request, 'projects/about.html', context)
+    return render(request, 'projects/about.html')
+    
 
 def project(request):
     return render(request, 'projects/projects.html')
